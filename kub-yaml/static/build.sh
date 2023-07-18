6dbbb1e7ce802f0729aa36068f319b5522cee411
Ray Chung <rchung725@gmail.com>
2023-07-18 01:07:52 +0000

    testing v0.2

--- kub-yaml/static/build.sh
+++ kub-yaml/static/build.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+# Temp, removing old pod
+kubectl delete pod home-web2-pod
+
 kubectl apply -f nginx-pod.yaml
 kubectl apply -f nginx-pod-service.yaml
+
+
+# If pod is annoying, ssh to it:
+# kubectl exec -it <pod-name> -n <namespace> -- bash
+#   - https://stackoverflow.com/questions/45714658/need-to-do-ssh-to-kubernetes-pod
